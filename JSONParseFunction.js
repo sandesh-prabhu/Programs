37e4954cf35ec47f452dbb6c24982595a06356df
unknown <sandesh.prabhu@robosoftin.com>
2022-08-29 12:49:28 +0530

    Added stringify function

--- JSONParseFunction.js
+++ JSONParseFunction.js
@@ -1,6 +1,6 @@
 let obj=JSON.parse('{"FirstName":"Hello","LastName":"World"}')
 console.log(obj.FirstName+" "+obj.LastName)
-
+//Hello World
 
 let text='{"name":"John","gender":"Male"}'
 let obj2=JSON.parse(text,(key,value)=>{
@@ -13,9 +13,8 @@ let obj2=JSON.parse(text,(key,value)=>{
     else
         return value
 })
-
 console.log(obj2.gender+" is "+obj2.name)
-
+//He is John
 
 let obj3={State:"Karnataka",City:{Name:"Udupi",Code:20}}
 let obj4=JSON.parse(JSON.stringify(obj3))
@@ -23,3 +22,6 @@ obj4.City.Code=21
 console.log(obj3)
 console.log(obj4)
 console.log(typeof(JSON.stringify(obj3)))
+//{ State: 'Karnataka', City: { Name: 'Udupi', Code: 20 } }
+//{ State: 'Karnataka', City: { Name: 'Udupi', Code: 21 } }
+//string
