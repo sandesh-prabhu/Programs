7f6a266ea90a8f531dad58853e821a9136e19ec5
SandeshPrabhuRobosoft <111508194+SandeshPrabhuRobosoft@users.noreply.github.com>
2022-08-29 11:59:31 +0530

    Delete NodeJS directory

--- NodeJS/nodejs programs/access_file_system.js
+++
@@ -1,8 +0,0 @@
-const http=require('http')
-const fs=require('fs')
-http.createServer((req,res)=>{
-    fs.readFile('access_file_system.html',(err,data)=>{
-        res.write(data)
-        return res.end()
-    })
-}).listen(8080)
